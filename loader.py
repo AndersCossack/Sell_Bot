3f4c46c94137d68e6af141e927bd0b234113be8e
Anders <aspidcg@gmail.com>
2025-05-12 19:29:02 +0300

    First Bot

--- loader.py
+++ loader.py
@@ -2,8 +2,6 @@ from aiogram import Bot, Dispatcher
 import os
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='data/config.env')
-API_BOT = os.getenv("API_BOT")
-
-bot = Bot(token=API_BOT)
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher()
