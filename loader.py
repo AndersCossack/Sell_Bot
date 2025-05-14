4a5040a85035f19ff06fd3b448ea9292edc409ed
AndersCossack <aspidcg@gmail.com>
2025-05-14 10:55:56 +0300

    hotfix

--- loader.py
+++ loader.py
@@ -2,6 +2,11 @@ from aiogram import Bot, Dispatcher
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-bot = Bot(os.getenv('TOKEN'))
+load_dotenv(override=True)
+
+token = os.getenv('TOKEN')
+if not token:
+    raise ValueError('Token not found! Check your .env file and make sure TOKEN is set.')
+
+bot = Bot(token)
 dp = Dispatcher()
